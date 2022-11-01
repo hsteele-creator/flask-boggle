e305ab80b8cf344a48066dc07c7dc97e3fec1704
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2022-11-01 17:04:41 -0400

    finished number 2

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from boggle import Boggle
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 app = Flask(__name__)
 boggle_game = Boggle()
 
+app.config['SECRET_KEY'] = "secret key"
+
 @app.route("/")
 def display_board():
+    session["board"] = Boggle.make_board(Boggle)
     return render_template("board.html", board = Boggle.make_board(Boggle))
+    
