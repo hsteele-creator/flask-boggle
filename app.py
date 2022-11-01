8b83afcefc66c4f9163ed1b647f7737b3c544be3
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2022-11-01 09:54:17 -0400

    working on number 1

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 from boggle import Boggle
+from flask import Flask, render_template
 
+app = Flask(__name__)
 boggle_game = Boggle()
+
+@app.route("/")
+def display_board():
+    return render_template("board.html", board = Boggle.make_board(Boggle))
