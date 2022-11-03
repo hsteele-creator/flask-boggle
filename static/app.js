78ea05c9c0ddfd9eec5432cf6f89d4f56cc2e646
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2022-11-03 17:58:52 -0400

    added requirements.txt

--- static/app.js
+++ static/app.js
@@ -2,3 +2,8 @@ $("#submit-guess").on("submit", function(e) {
     e.preventDefault()
     console.log("hello")
 })
+
+$("#submit-guess").prop("disabled", true);
+setTimeout(function() {
+   $("#submit-guess").prop("disabled", false);
+}, 60000);
